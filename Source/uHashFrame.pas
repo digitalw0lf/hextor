8eddd93469b49b3c290ca50ffe3c1c729144daef
Grigoriy Mylnikov <dwf@hextor.net>
2020-10-11 16:42:02 +0800

    Replace HashLib! with HashLib4Pascal

--- Source/uHashFrame.pas
+++ Source/uHashFrame.pas
@@ -16,7 +16,7 @@ uses
   Vcl.StdCtrls, Vcl.CheckLst, Vcl.ComCtrls, Generics.Collections, Vcl.Menus,
   Clipbrd, Math,
 
-  CryptoAPI, uHextorTypes, uEditorForm;
+  {CryptoAPI,} HlpHashFactory, HlpIHash, HlpCRC, uHextorTypes, uEditorForm;
 
 type
   EHashException = class(Exception);
@@ -30,30 +30,42 @@ type
     procedure Update(Data: Pointer; Size: Integer); virtual; abstract;
     procedure Done(var Result: string); virtual; abstract;
     procedure Calculate(Data: Pointer; Size: Integer; var Res: string);
-    procedure Assign(Source: THashAlgorithm); virtual;
+//    procedure Assign(Source: THashAlgorithm); virtual;
   end;
 
-  // Hash function from HashLib! library
-  THashLibHash = class(THashAlgorithm)
+//  // Hash function from HashLib! library
+//  THashLibHash = class(THashAlgorithm)
+//  private
+//    Context: THashContext;
+//  public
+//    HashType: LongWord;
+//    procedure Init(); override;
+//    procedure Update(Data: Pointer; Size: Integer); override;
+//    procedure Done(var Result: string); override;
+////    procedure Assign(Source: THashAlgorithm); override;
+//  end;
+//
+//  // CRC16/CCITT
+//  TCRC16Hash = class(THashAlgorithm)
+//  private
+//    Value: Word;
+//  public
+//    procedure Init(); override;
+//    procedure Update(Data: Pointer; Size: Integer); override;
+//    procedure Done(var Result: string); override;
+//  end;
+
+  // Hash function from HashLib4Pascal library
+  THashLib4PascalHash = class(THashAlgorithm)
   private
-    Context: THashContext;
+    Hash: IHash;
   public
-    HashType: LongWord;
     procedure Init(); override;
     procedure Update(Data: Pointer; Size: Integer); override;
     procedure Done(var Result: string); override;
-    procedure Assign(Source: THashAlgorithm); override;
+    constructor Create(AName: string; AHash: IHash);
   end;
 
-  // CRC16/CCITT
-  TCRC16Hash = class(THashAlgorithm)
-  private
-    Value: Word;
-  public
-    procedure Init(); override;
-    procedure Update(Data: Pointer; Size: Integer); override;
-    procedure Done(var Result: string); override;
-  end;
 
   THashFrame = class(TFrame)
     Panel1: TPanel;
@@ -193,22 +205,22 @@ begin
   end;
 end;
 
-const
-  HashesAvail: array[0..19] of String = (
-    'MD2', 'MD4', 'MD5', 'SHA1', 'SHA256', 'SHA384',
-    'SHA512', 'HAVAL128', 'HAVAL160', 'HAVAL192',
-    'HAVAl224', 'HAVAL256', {'GOST',} 'TIGER128',
-    'TIGER160', 'TIGER192', 'RIPE-MD128', 'RIPE-MD160',
-    'CRC32', 'CRC32b', 'Adler32'
-  );
-
-  HashesAvailID: array[0..19] of LongWord = (
-    HASH_MD2, HASH_MD4, HASH_MD5, HASH_SHA1, HASH_SHA256, HASH_SHA384,
-    HASH_SHA512, HASH_HAVAL128, HASH_HAVAL160, HASH_HAVAL192,
-    HASH_HAVAl224, HASH_HAVAL256, {HASH_GOST,} HASH_TIGER128,
-    HASH_TIGER160, HASH_TIGER192, HASH_RIPEMD128, HASH_RIPEMD160,
-    HASH_CRC32, HASH_CRC32B, HASH_ADLER32
-  );
+//const
+//  HashesAvail: array[0..19] of String = (
+//    'MD2', 'MD4', 'MD5', 'SHA1', 'SHA256', 'SHA384',
+//    'SHA512', 'HAVAL128', 'HAVAL160', 'HAVAL192',
+//    'HAVAl224', 'HAVAL256', {'GOST',} 'TIGER128',
+//    'TIGER160', 'TIGER192', 'RIPE-MD128', 'RIPE-MD160',
+//    'CRC32', 'CRC32b', 'Adler32'
+//  );
+//
+//  HashesAvailID: array[0..19] of LongWord = (
+//    HASH_MD2, HASH_MD4, HASH_MD5, HASH_SHA1, HASH_SHA256, HASH_SHA384,
+//    HASH_SHA512, HASH_HAVAL128, HASH_HAVAL160, HASH_HAVAL192,
+//    HASH_HAVAl224, HASH_HAVAL256, {HASH_GOST,} HASH_TIGER128,
+//    HASH_TIGER160, HASH_TIGER192, HASH_RIPEMD128, HASH_RIPEMD160,
+//    HASH_CRC32, HASH_CRC32B, HASH_ADLER32
+//  );
 
 constructor THashFrame.Create(AOwner: TComponent);
 begin
@@ -254,18 +266,39 @@ begin
 end;
 
 procedure THashFrame.RegisterBuiltInHashes;
-var
-  i: Integer;
-  HLHash: THashLibHash;
+//var
+//  i: Integer;
+//  HLHash: THashLibHash;
 begin
-  for i:=Low(HashesAvail) to High(HashesAvail) do
-  begin
-    HLHash := THashLibHash.Create(HashesAvail[i]);
-    HLHash.HashType := HashesAvailID[i];
-    HashTypes.Add(HLHash);
-  end;
+//  for i:=Low(HashesAvail) to High(HashesAvail) do
+//  begin
+//    HLHash := THashLibHash.Create('!'+HashesAvail[i]);
+//    HLHash.HashType := HashesAvailID[i];
+//    HashTypes.Add(HLHash);
+//  end;
+//
+//  HashTypes.Add(TCRC16Hash.Create('?CRC16/CCITT'));
 
-  HashTypes.Add(TCRC16Hash.Create('CRC16/CCITT'));
+  HashTypes.Add(THashLib4PascalHash.Create('CRC16-CCITT-FALSE',  THashFactory.TChecksum.TCRC.CreateCRC(TCRCStandard.CRC16_CCITTFALSE) ));
+  HashTypes.Add(THashLib4PascalHash.Create('CRC16-X25',    THashFactory.TChecksum.TCRC.CreateCRC(TCRCStandard.X25) ));
+  HashTypes.Add(THashLib4PascalHash.Create('CRC16-XMODEM', THashFactory.TChecksum.TCRC.CreateCRC(TCRCStandard.XMODEM) ));
+  HashTypes.Add(THashLib4PascalHash.Create('CRC32',        THashFactory.TChecksum.TCRC.CreateCRC32_PKZIP() ));
+  HashTypes.Add(THashLib4PascalHash.Create('CRC32-C',      THashFactory.TChecksum.TCRC.CreateCRC32_CASTAGNOLI() ));
+  HashTypes.Add(THashLib4PascalHash.Create('CRC64',        THashFactory.TChecksum.TCRC.CreateCRC64_ECMA_182() ));
+
+  HashTypes.Add(THashLib4PascalHash.Create('MD2',          THashFactory.TCrypto.CreateMD2() ));
+  HashTypes.Add(THashLib4PascalHash.Create('MD4',          THashFactory.TCrypto.CreateMD4() ));
+  HashTypes.Add(THashLib4PascalHash.Create('MD5',          THashFactory.TCrypto.CreateMD5() ));
+
+  HashTypes.Add(THashLib4PascalHash.Create('SHA-1',        THashFactory.TCrypto.CreateSHA1() ));
+  HashTypes.Add(THashLib4PascalHash.Create('SHA-256',      THashFactory.TCrypto.CreateSHA2_256() ));
+  HashTypes.Add(THashLib4PascalHash.Create('SHA-384',      THashFactory.TCrypto.CreateSHA2_384() ));
+  HashTypes.Add(THashLib4PascalHash.Create('SHA-512',      THashFactory.TCrypto.CreateSHA2_512() ));
+  HashTypes.Add(THashLib4PascalHash.Create('SHA-3-256',    THashFactory.TCrypto.CreateSHA3_256() ));
+
+  HashTypes.Add(THashLib4PascalHash.Create('Adler32',      THashFactory.TChecksum.CreateAdler32() ));
+  HashTypes.Add(THashLib4PascalHash.Create('RIPEMD-160',   THashFactory.TCrypto.CreateRIPEMD160() ));
+
 end;
 
 procedure THashFrame.ShowAlgorithmsList;
@@ -287,10 +320,10 @@ end;
 
 { THashAlgorithm }
 
-procedure THashAlgorithm.Assign(Source: THashAlgorithm);
-begin
-  Name := Source.Name;
-end;
+//procedure THashAlgorithm.Assign(Source: THashAlgorithm);
+//begin
+//  Name := Source.Name;
+//end;
 
 procedure THashAlgorithm.Calculate(Data: Pointer; Size: Integer;
   var Res: string);
@@ -306,83 +339,108 @@ begin
   Name := AName;
 end;
 
-{ THashLibHash }
+//{ THashLibHash }
+//
+////procedure THashLibHash.Assign(Source: THashAlgorithm);
+////begin
+////  inherited;
+////  if Source is THashLibHash then
+////  begin
+////    HashType := (Source as THashLibHash).HashType;
+////  end;
+////end;
+//
+//procedure THashLibHash.Done(var Result: string);
+//var
+//  Res: Cardinal;
+//begin
+//  Res := HashFinal(@Context, Result);
+//  if Res <> HASH_NOERROR then
+//    raise EHashException.Create(HashErrorToStr(Res));
+//end;
+//
+//procedure THashLibHash.Init;
+//var
+//  Res: Cardinal;
+//begin
+//  Res := HashInit(@Context, HashType);
+//  if Res <> HASH_NOERROR then
+//    raise EHashException.Create(HashErrorToStr(Res));
+//end;
+//
+//procedure THashLibHash.Update(Data: Pointer; Size: Integer);
+//var
+//  Res: Cardinal;
+//begin
+//  Res := HashUpdate(@Context, Data, Size);
+//  if Res <> HASH_NOERROR then
+//    raise EHashException.Create(HashErrorToStr(Res));
+//end;
+//
+//{ TCRC16Hash }
+//
+//procedure TCRC16Hash.Done(var Result: string);
+//begin
+//  Value := not Value;
+//  Result := IntToHex(Value, 4);
+//end;
+//
+//procedure TCRC16Hash.Init;
+//begin
+//  Value := $FFFF;
+//end;
+//
+//procedure TCRC16Hash.Update(Data: Pointer; Size: Integer);
+//var
+//  i: Integer;
+//  crc_hi,crc_lo: Byte;
+//  t: Word;
+//  v, X: Byte;
+//begin
+//  crc_hi:=Value shr 8; crc_lo:=Value and 255;
+//  for i:=0 to Size-1 do
+//  begin
+//    X := PByteArray(Data)^[i];
+//    t:=X xor crc_lo;
+//    for v:=1 to 8 do
+//    begin
+//      if (t and 1)=1 then
+//      begin
+//        t:=t shr 1;
+//        t:=t xor $8408;
+//      end
+//      else
+//        t:=t shr 1;
+//    end;
+//    crc_lo:=t xor crc_hi;
+//    crc_hi:=t shr 8;
+//  end;
+//  Value := crc_lo + (crc_hi shl 8);
+//end;
 
-procedure THashLibHash.Assign(Source: THashAlgorithm);
+{ THashLib4PascalHash }
+
+constructor THashLib4PascalHash.Create(AName: string; AHash: IHash);
 begin
-  inherited;
-  if Source is THashLibHash then
-  begin
-    HashType := (Source as THashLibHash).HashType;
-  end;
+  if AName = '' then
+    AName := AHash.Name;
+  inherited Create(AName);
+  Hash := AHash;
 end;
 
-procedure THashLibHash.Done(var Result: string);
-var
-  Res: Cardinal;
+procedure THashLib4PascalHash.Done(var Result: string);
 begin
-  Res := HashFinal(@Context, Result);
-  if Res <> HASH_NOERROR then
-    raise EHashException.Create(HashErrorToStr(Res));
+  Result := Hash.TransformFinal.ToString();
 end;
 
-procedure THashLibHash.Init;
-var
-  Res: Cardinal;
+procedure THashLib4PascalHash.Init;
 begin
-  Res := HashInit(@Context, HashType);
-  if Res <> HASH_NOERROR then
-    raise EHashException.Create(HashErrorToStr(Res));
+  Hash.Initialize();
 end;
 
-procedure THashLibHash.Update(Data: Pointer; Size: Integer);
-var
-  Res: Cardinal;
+procedure THashLib4PascalHash.Update(Data: Pointer; Size: Integer);
 begin
-  Res := HashUpdate(@Context, Data, Size);
-  if Res <> HASH_NOERROR then
-    raise EHashException.Create(HashErrorToStr(Res));
-end;
-
-{ TCRC16Hash }
-
-procedure TCRC16Hash.Done(var Result: string);
-begin
-  Value := not Value;
-  Result := IntToHex(Value, 4);
-end;
-
-procedure TCRC16Hash.Init;
-begin
-  Value := $FFFF;
-end;
-
-procedure TCRC16Hash.Update(Data: Pointer; Size: Integer);
-var
-  i: Integer;
-  crc_hi,crc_lo: Byte;
-  t: Word;
-  v, X: Byte;
-begin
-  crc_hi:=Value shr 8; crc_lo:=Value and 255;
-  for i:=0 to Size-1 do
-  begin
-    X := PByteArray(Data)^[i];
-    t:=X xor crc_lo;
-    for v:=1 to 8 do
-    begin
-      if (t and 1)=1 then
-      begin
-        t:=t shr 1;
-        t:=t xor $8408;
-      end
-      else
-        t:=t shr 1;
-    end;
-    crc_lo:=t xor crc_hi;
-    crc_hi:=t shr 8;
-  end;
-  Value := crc_lo + (crc_hi shl 8);
+  Hash.TransformUntyped(Data^, Size);
 end;
 
 end.
